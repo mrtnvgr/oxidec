9e8056cdd183a640cf950f7a825a1602d77da4fc
mrtnvgr <martynovegorOF@yandex.ru>
2023-05-23 23:01:48 +0700

    fix: remove unused import

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,6 @@
 use home::home_dir;
 use rand::seq::IteratorRandom;
-use std::{
-    fs, io,
-    path::{Path, PathBuf},
-};
+use std::{fs, io, path::PathBuf};
 use strum::{EnumIter, IntoEnumIterator};
 
 #[derive(EnumIter)]
