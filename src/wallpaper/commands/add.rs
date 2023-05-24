e31acc774f3bd7442bc1ed48b86fe4f218bc782b
mrtnvgr <martynovegorOF@yandex.ru>
2023-05-24 16:12:37 +0700

    feat(wallpaper): check if file is a photo

--- src/wallpaper/commands/add.rs
+++ src/wallpaper/commands/add.rs
@@ -1,8 +1,11 @@
 use crate::{config::Folder, wallpaper::args};
+use file_format::{FileFormat, Kind};
 
 pub fn handle(args: &args::Add) {
-    // TODO: check if file is a photo
     assert!(args.file_path.exists(), "This file does not exist");
+
+    let format = FileFormat::from_file(&args.file_path).unwrap();
+    assert!(format.kind() == Kind::Image, "This file is not an image");
 
     let filename = args.file_path.file_name().unwrap();
     let name = filename.to_str().unwrap();
