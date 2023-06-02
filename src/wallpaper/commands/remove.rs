3dfa17351cff8c4bd87873c6059410ff679cdffc
mrtnvgr <martynovegorOF@yandex.ru>
2023-06-02 11:36:35 +0700

    fix(clippy): remove needless borrow

--- src/wallpaper/commands/remove.rs
+++ src/wallpaper/commands/remove.rs
@@ -3,7 +3,10 @@ use crate::{config::Folder, theme::schema, wallpaper::args};
 pub fn handle(args: &args::Remove) {
     let name = args.filename.file_name().unwrap().to_str().unwrap();
 
-    assert!(Folder::Wallpapers.contains(&name), "This wallpaper does not exist");
+    assert!(
+        Folder::Wallpapers.contains(name),
+        "This wallpaper does not exist"
+    );
 
     for path in Folder::Themes.list() {
         let theme = schema::Theme::from_file(&path).unwrap();
