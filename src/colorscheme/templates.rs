3578352b3f8fd4c9271b193051c6c7ba4da6edee
mrtnvgr <martynovegorOF@yandex.ru>
2023-06-11 21:41:25 +0700

    feat(colorscheme): add "lighten" filter for templates

--- src/colorscheme/templates.rs
+++ src/colorscheme/templates.rs
@@ -1,3 +1,4 @@
+use colorsys::{ColorTransform, Rgb};
 use upon::Engine;
 
 use super::schema;
@@ -5,13 +6,9 @@ use crate::{cache, config};
 use std::fs;
 
 pub fn generate(colorscheme: &schema::Colorscheme) {
-    let mut engine = Engine::new();
-    engine.add_filter("strip", |text: String| {
-        text.trim_start_matches('#').to_owned()
-    });
+    let engine = get_engine();
 
     let templates = config::Folder::Templates.list();
-
     assert!(!templates.is_empty(), "No templates for generation.");
 
     for path in templates {
@@ -23,4 +20,20 @@ pub fn generate(colorscheme: &schema::Colorscheme) {
         let template_name: &str = path.file_name().unwrap().to_str().unwrap();
         cache::templates::create(template_name, result).unwrap();
     }
+}
+
+fn get_engine() -> Engine<'static> {
+    let mut engine = Engine::new();
+
+    engine.add_filter("strip", |text: String| {
+        text.trim_start_matches('#').to_owned()
+    });
+
+    engine.add_filter("lighten", |text: String, amt: f64| {
+        let mut color = Rgb::from_hex_str(&text).unwrap();
+        color.lighten(amt);
+        color.to_hex_string()
+    });
+
+    engine
 }
