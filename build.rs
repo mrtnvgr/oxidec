8db3a3564f988b1c6c25a05b671d809de2961a40
mrtnvgr <martynovegorOF@yandex.ru>
2023-06-12 20:23:05 +0700

    feat: improve build script

--- build.rs
+++ build.rs
@@ -1,12 +1,14 @@
 use std::{
     env,
     ffi::OsString,
+    fs,
     path::{Path, PathBuf},
     process::Command,
 };
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=examples");
 
     #[cfg(windows)]
     panic!("This program is Linux only.");
@@ -18,14 +20,26 @@ fn main() {
 
 fn copy_example_folder(home_dir: &OsString) {
     let config_path = Path::new(&home_dir).join(".config/oxidec/");
-    if !config_path.exists() {
-        copy_folder("examples", config_path);
+    let repo_path = Path::new("examples");
+
+    for folder in ["templates", "reloaders"] {
+        let user_folder = config_path.join(folder);
+        let repo_folder = repo_path.join(folder);
+
+        for repo_file in fs::read_dir(repo_folder).unwrap().flatten() {
+            let file_name = repo_file.file_name();
+            let user_file = user_folder.join(file_name);
+
+            if !user_file.exists() {
+                copy_file(repo_file.path(), user_file);
+            }
+        }
     }
 }
 
-fn copy_folder(src: &str, dest: PathBuf) {
+fn copy_file(src: PathBuf, dest: PathBuf) {
     Command::new("cp")
-        .arg("-r")
+        .arg("-n")
         .arg(src)
         .arg(dest)
         .output()
