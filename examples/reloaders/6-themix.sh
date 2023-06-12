a50229c6bad85c8f2c18f63312be4b05f3619f8b
mrtnvgr <martynovegorOF@yandex.ru>
2023-06-12 19:10:11 +0700

    feat(colorscheme): improve themix integration

--- examples/reloaders/6-themix.sh
+++ examples/reloaders/6-themix.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 THEMIX_PATH="/opt/oomox"
+GTK_THEMES_PATH="${HOME}/.themes"
+GTK_ICONS_PATH="${HOME}/.icons"
 
 if [ ! -d "${THEMIX_PATH}" ]; then
     exit 0
@@ -11,7 +13,15 @@ THEMIX_PLUGINS_PATH="${THEMIX_PATH}/plugins"
 THEMIX_THEME_PATH="${THEMIX_PLUGINS_PATH}/theme_oomox"
 THEMIX_ICONS_PATH="${THEMIX_PLUGINS_PATH}/icons_papirus"
 
-TEMPLATE_PATH="${HOME}/.cache/oxidec/templates/colors-themix"
+OXIDEC_CACHE_PATH="${HOME}/.cache/oxidec"
+TEMPLATE_PATH="${OXIDEC_CACHE_PATH}/templates/colors-themix"
 
-${THEMIX_THEME_PATH}/change_color.sh -o oxidec ${TEMPLATE_PATH} &>/dev/null &
-${THEMIX_ICONS_PATH}/change_color.sh -o oxidec ${TEMPLATE_PATH} &>/dev/null &
+if [ ! -d "${GTK_THEMES_PATH}/${OXIDEC_COLORSCHEME}" ]; then
+    echo "! Generating GTK theme. This may take a while."
+    ${THEMIX_THEME_PATH}/change_color.sh -o ${OXIDEC_COLORSCHEME} ${TEMPLATE_PATH} &>/dev/null
+fi
+
+if [ ! -d "${GTK_ICONS_PATH}/${OXIDEC_COLORSCHEME}" ]; then
+    echo "! Generating GTK icons. This may take a while."
+    ${THEMIX_ICONS_PATH}/change_color.sh -o ${OXIDEC_COLORSCHEME} ${TEMPLATE_PATH} &>/dev/null
+fi
--- src/colorscheme/reloaders.rs
+++ src/colorscheme/reloaders.rs
@@ -1,9 +1,16 @@
-use crate::config::Folder;
+use crate::{cache::status::Colorscheme, config::Folder};
 use std::process::Command;
 
 pub fn run() {
+    let colorscheme = Colorscheme::load().name;
+
     for reloader in Folder::Reloaders.list() {
-        let status = Command::new("sh").arg("-C").arg(&reloader).status();
+        let mut command = Command::new("sh");
+
+        command.arg("-C").arg(&reloader);
+        command.env("OXIDEC_COLORSCHEME", &colorscheme);
+
+        let status = command.status();
 
         if !status.unwrap().success() {
             log::error!("Error occured in {:?}", reloader.file_name().unwrap());
