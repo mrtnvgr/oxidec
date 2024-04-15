52a76b0df086aee06f1a0447b39a1b4f1452c631
mrtnvgr <root@unixis.fun>
2024-04-15 19:49:40 +0700

    feat(nix): add templates, fix wallpapers

--- nix/module.nix
+++ nix/module.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }:
 let
-  inherit (lib) mkIf mkEnableOption mkOption mapAttrs mergeAttrsList;
+  inherit (lib) mkIf mkEnableOption mkOption mapAttrs mergeAttrsList optionalString listToAttrs;
 
   pkg = pkgs.rustPlatform.buildRustPackage {
     name = "oxidec";
@@ -13,7 +13,18 @@ let
 
   types = lib.types // (import ./types.nix { inherit lib; });
 
-  mkFiles = group: mapAttrs (name: value: { text = builtins.toJSON value; target = "oxidec/${group}/${name}.json"; }) config.oxidec.${group};
+  mkJSONFile = group: mapAttrs (name: value: { text = builtins.toJSON value; target = "oxidec/${group}/${name}.json"; }) config.oxidec.${group};
+  JSONFiles = mergeAttrsList (map (x: mkJSONFile x) [ "colorschemes" "themes" ]);
+
+  wallpapers = listToAttrs (map (wallpaper: {
+    name = "oxidec/wallpapers/${wallpaper.name}";
+    value = { source = wallpaper; };
+  }) config.oxidec.wallpapers);
+
+  mkTextFile = group: mapAttrs (name: value: { text = value; target = "oxidec/${group}/${name}"; }) config.oxidec.${group};
+  textFiles = mergeAttrsList (map (x: mkTextFile x) [ "templates" ]);
+
+  oxidecFiles = JSONFiles // textFiles // wallpapers;
 in {
   options.oxidec = {
     enable = mkEnableOption "enable oxidec";
@@ -37,7 +48,7 @@ in {
     };
 
     wallpapers = mkOption {
-      type = with types; listOf wallpaper;
+      type = with types; listOf path;
       default = [ ];
 
       # TODO: example = [];
@@ -49,16 +60,22 @@ in {
 
       # TODO: example = {};
     };
+
+    templates = mkOption {
+      type = with types; attrsOf str;
+      default = {};
+
+      # TODO: example = {};
+    };
   };
 
   config = mkIf cfg.enable {
     home.packages = [ pkg ];
 
-    xdg.configFile = mergeAttrsList (map (x: mkFiles x) [ "colorschemes" "themes" ]);
+    xdg.configFile = oxidecFiles;
 
     home.shellAliases = mapAttrs (n: v: "oxidec ${v}") cfg.aliases;
 
-    # TODO: templates
     # TODO: activation scripts
   };
 }
