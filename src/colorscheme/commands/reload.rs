e142ff2b7a4abf388b3538c4ff0ff4915181caaa
mrtnvgr <martynovegorOF@yandex.ru>
2026-07-31 02:19:46 +0700

    fix: make colorscheme reloads re-generate templates too

---
+++ src/colorscheme/commands/reload.rs
@@ -0,0 +1,11 @@
+use crate::cache::status::{Colorscheme, Object};
+use crate::colorscheme::{schema, set_without_cache};
+
+pub fn handle() {
+    if let Some(status) = Colorscheme::try_load() {
+        let colorscheme = schema::Colorscheme::from_file(&status.path).unwrap();
+        set_without_cache(&colorscheme);
+    } else {
+        log::warn!("No colorscheme to reload");
+    }
+}
