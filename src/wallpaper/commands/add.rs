7de7fdf86bcc0bb49c43ef35a75091e9b22670d1
mrtnvgr <martynovegorOF@yandex.ru>
2026-08-15 02:14:11 +0700

    perf: replace file-format with magic-byte checking

--- src/wallpaper/commands/add.rs
+++ src/wallpaper/commands/add.rs
@@ -1,11 +1,14 @@
 use crate::{config::Directory, wallpaper::args};
-use file_format::{FileFormat, Kind};
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
 
 pub fn handle(args: &args::Add) {
     assert!(args.file_path.exists(), "This file does not exist");
-
-    let format = FileFormat::from_file(&args.file_path).unwrap();
-    assert!(format.kind() == Kind::Image, "This file is not an image");
+    assert!(
+        is_image(&args.file_path),
+        "This file is not an image"
+    );
 
     let filename = args.file_path.file_name().unwrap();
     let name = filename.to_str().unwrap();
@@ -16,4 +19,18 @@ pub fn handle(args: &args::Add) {
 
     Directory::Wallpapers.copy(&args.file_path).unwrap();
     log::info!("Added successfully!");
+}
+
+fn is_image(path: &Path) -> bool {
+    let mut magic = [0u8; 16];
+    let mut file = File::open(path).unwrap();
+    if file.read_exact(&mut magic).is_err() {
+        return false;
+    }
+
+    let png = magic.starts_with(b"\x89PNG\r\n\x1a\n");
+    let jpeg = magic.starts_with(b"\xFF\xD8\xFF");
+    let bmp = magic.starts_with(b"BM");
+
+    png || jpeg || bmp
 }
